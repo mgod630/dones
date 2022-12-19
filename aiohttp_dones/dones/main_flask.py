0f8ae4f451dba80472a05961e0dfe029d951ea5e
amirhossein <mgod630@gmail.com>
2022-12-19 14:57:00 +0330

    first

---
+++ aiohttp_dones/dones/main_flask.py
@@ -0,0 +1,33 @@
+import os
+import secrets
+from flask import Flask, request, redirect
+import mysql.connector.pooling
+from routes import make_routes, common
+
+
+def init(config_file="settings.py"):
+    app = Flask(__name__)
+    app.config.from_pyfile(config_file)
+    app.config["UPLOAD_FOLDER"] = os.path.join("static", "uploaded_files")
+    app.config["MAX_CONTENT_LENGTH"] = 16 * 1000 * 1000
+    app.config["SECRET_KEY"] = 'secrets.token_hex(16)'
+    # TODO Use pure in production
+    app.config['mysql_connection_pool'] = mysql.connector.pooling.MySQLConnectionPool(
+        user="root", password="", database='goldis', use_pure=True, pool_name="my_pool", pool_size=32, buffered=True)
+    no_login_need_urls = ["/login", "/login-post", "/signup", "/signup-post"]
+    goldis_blueprint = make_routes()
+    app.register_blueprint(goldis_blueprint)
+
+    @app.before_request
+    def before_request_func():
+        # new_response = redirect('/login')d
+        new_response = None
+        if request.path in no_login_need_urls or common.get_user_from_token() != None:
+            new_response = None
+        return new_response
+    return app
+
+
+if __name__ == "__main__" and True:
+    app = init()
+    app.run(port=5002)
