06c4836b0a9b8760274c6a9c550a2ce73aea7850
NedaAlipour39 <neda.alipour39@gmail.com>
2022-12-24 15:37:27 +0330

    changes

--- aiohttp_dones/dones/models_mysql/emails_orm.py
+++ aiohttp_dones/dones/models_mysql/emails_orm.py
@@ -1,119 +1,106 @@
 from flask import current_app as app
 import mysql.connector.pooling
-import uuid
 
 connection_pool = None
 
-class Notifications:
+class Emails:
     @staticmethod
-    def get_all_notifications():
+    def get_all_emails():
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor(dictionary=True)
-        query = ("SELECT * FROM tbl_notifications ")
+        query = ("SELECT * FROM tbl_emails ")
         cursor.execute(query)
         data = cursor.fetchall()
         cnx.close()
         return data
 
     @staticmethod
-    def get_notifications_by_section_id(section_id):
+    def get_emails_by_section_id(section_id):
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor(dictionary=True)
-        query = "SELECT * FROM tbl_notifications WHERE section_id=%(section_id)s"
+        query = "SELECT * FROM tbl_emails WHERE section_id=%(section_id)s"
         cursor.execute(query, {'section_id': section_id})
         row = cursor.fetchall()
         cnx.close()
         return row
 
     @staticmethod
-    def get_notification_by_id(id):
+    def get_email_by_id(id):
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor(dictionary=True)
-        query = "SELECT * FROM tbl_notifications WHERE id=%(id)s"
+        query = "SELECT * FROM tbl_emails WHERE id=%(id)s"
         cursor.execute(query, {'id': id})
         row = cursor.fetchone()
         cnx.close()
         return row
 
     @staticmethod
-    def insert_new_notification(section_id, jalali_date, notification_type, notification_text):
+    def insert_new_email(email):
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor(dictionary=True)
-        add_notification = ("INSERT INTO `tbl_notifications` (`section_id`, `jalali_date`, `notification_type`, `notification_text`) VALUES" +
-                      "( %(section_id)s, %(jalali_date)s, %(notification_type)s, %(notification_text)s)")
-        data_notification = {
-            'section_id': section_id,
-            'jalali_date': jalali_date,
-            'notification_type': notification_type,
-            'notification_text': notification_text,
+        add_email = ("INSERT INTO `tbl_emails` (`email`) VALUES" +
+                      "( %(email)s)")
+        data_email = {
+            'email': email,
         }
-        cursor.execute(add_notification, data_notification)
+        cursor.execute(add_email, data_email)
         inserted_record_id = cursor.lastrowid
         cnx.commit()
         cnx.close()
         return inserted_record_id
 
     @staticmethod
-    def update_notification(id, section_id, jalali_date, notification_type, notification_text):
+    def update_email(id, email):
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor()
         update_string = ''
-        if section_id:
-            update_string += f'section_id=%(section_id)s,'
-        if jalali_date:
-            update_string += f'jalali_date=%(jalali_date)s,'
-        if notification_type:
-            update_string += f'notification_type=%(notification_type)s,'
-        if notification_text:
-            update_string += f'notification_text=%(notification_text)s,'
+        if email:
+            update_string += f'email=%(email)s,'
         update_string = update_string.rstrip(',')
-        add_notification = f"UPDATE tbl_notifications SET {update_string} WHERE id='{id}'"
-        data_notification = {
-            'section_id': section_id,
-            'jalali_date': jalali_date,
-            'notification_type': notification_type,
-            'notification_text': notification_text,
+        add_email = f"UPDATE tbl_emails SET {update_string} WHERE id='{id}'"
+        data_email = {
+            'email': email,
         }
-        cursor.execute(add_notification, data_notification)
+        cursor.execute(add_email, data_email)
         cnx.commit()
         cnx.close()
         return True
 
     @staticmethod
-    def delete_all_notifications():
+    def delete_all_emails():
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor()
-        cursor.execute('DELETE FROM tbl_notifications')
+        cursor.execute('DELETE FROM tbl_emails')
         cnx.commit()
         cnx.close()
         return True
 
     @staticmethod
-    def delete_notification_by_id(id):
+    def delete_email_by_id(id):
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor()
-        query = "DELETE FROM tbl_notifications WHERE id=%(id)s"
+        query = "DELETE FROM tbl_emails WHERE id=%(id)s"
         cursor.execute(query, {'id': id})
         cnx.commit()
         cnx.close()
@@ -123,4 +110,3 @@ class Notifications:
 if __name__ == '__main__':
     connection_pool = mysql.connector.pooling.MySQLConnectionPool(
         user="root", password="", database='goldis', use_pure=False, pool_name="my_pool", pool_size=32, buffered=True)
-
--- aiohttp_dones/dones/models_mysql/notices_orm.py
+++ aiohttp_dones/dones/models_mysql/notices_orm.py
@@ -1,72 +1,71 @@
 from flask import current_app as app
 import mysql.connector.pooling
-import uuid
 
 connection_pool = None
 
-class Notifications:
+class Notices:
     @staticmethod
-    def get_all_notifications():
+    def get_all_notices():
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor(dictionary=True)
-        query = ("SELECT * FROM tbl_notifications ")
+        query = ("SELECT * FROM tbl_notices ")
         cursor.execute(query)
         data = cursor.fetchall()
         cnx.close()
         return data
 
     @staticmethod
-    def get_notifications_by_section_id(section_id):
+    def get_notices_by_section_id(section_id):
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor(dictionary=True)
-        query = "SELECT * FROM tbl_notifications WHERE section_id=%(section_id)s"
+        query = "SELECT * FROM tbl_notices WHERE section_id=%(section_id)s"
         cursor.execute(query, {'section_id': section_id})
         row = cursor.fetchall()
         cnx.close()
         return row
 
     @staticmethod
-    def get_notification_by_id(id):
+    def get_notice_by_id(id):
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor(dictionary=True)
-        query = "SELECT * FROM tbl_notifications WHERE id=%(id)s"
+        query = "SELECT * FROM tbl_notices WHERE id=%(id)s"
         cursor.execute(query, {'id': id})
         row = cursor.fetchone()
         cnx.close()
         return row
 
     @staticmethod
-    def insert_new_notification(section_id, jalali_date, notification_type, notification_text):
+    def insert_new_notice(section_id, jalali_date, notice_type, notice_text):
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor(dictionary=True)
-        add_notification = ("INSERT INTO `tbl_notifications` (`section_id`, `jalali_date`, `notification_type`, `notification_text`) VALUES" +
-                      "( %(section_id)s, %(jalali_date)s, %(notification_type)s, %(notification_text)s)")
-        data_notification = {
+        add_notice = ("INSERT INTO `tbl_notices` (`section_id`, `jalali_date`, `notice_type`, `notice_text`) VALUES" +
+                      "( %(section_id)s, %(jalali_date)s, %(notice_type)s, %(notice_text)s)")
+        data_notice = {
             'section_id': section_id,
             'jalali_date': jalali_date,
-            'notification_type': notification_type,
-            'notification_text': notification_text,
+            'notice_type': notice_type,
+            'notice_text': notice_text,
         }
-        cursor.execute(add_notification, data_notification)
+        cursor.execute(add_notice, data_notice)
         inserted_record_id = cursor.lastrowid
         cnx.commit()
         cnx.close()
         return inserted_record_id
 
     @staticmethod
-    def update_notification(id, section_id, jalali_date, notification_type, notification_text):
+    def update_notice(id, section_id, jalali_date, notice_type, notice_text):
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
@@ -77,43 +76,43 @@ class Notifications:
             update_string += f'section_id=%(section_id)s,'
         if jalali_date:
             update_string += f'jalali_date=%(jalali_date)s,'
-        if notification_type:
-            update_string += f'notification_type=%(notification_type)s,'
-        if notification_text:
-            update_string += f'notification_text=%(notification_text)s,'
+        if notice_type:
+            update_string += f'notice_type=%(notice_type)s,'
+        if notice_text:
+            update_string += f'notice_text=%(notice_text)s,'
         update_string = update_string.rstrip(',')
-        add_notification = f"UPDATE tbl_notifications SET {update_string} WHERE id='{id}'"
-        data_notification = {
+        add_notice = f"UPDATE tbl_notices SET {update_string} WHERE id='{id}'"
+        data_notice = {
             'section_id': section_id,
             'jalali_date': jalali_date,
-            'notification_type': notification_type,
-            'notification_text': notification_text,
+            'notice_type': notice_type,
+            'notice_text': notice_text,
         }
-        cursor.execute(add_notification, data_notification)
+        cursor.execute(add_notice, data_notice)
         cnx.commit()
         cnx.close()
         return True
 
     @staticmethod
-    def delete_all_notifications():
+    def delete_all_notices():
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor()
-        cursor.execute('DELETE FROM tbl_notifications')
+        cursor.execute('DELETE FROM tbl_notices')
         cnx.commit()
         cnx.close()
         return True
 
     @staticmethod
-    def delete_notification_by_id(id):
+    def delete_notice_by_id(id):
         global connection_pool
         if connection_pool == None:
             connection_pool = app.config['mysql_connection_pool']
         cnx = connection_pool.get_connection()
         cursor = cnx.cursor()
-        query = "DELETE FROM tbl_notifications WHERE id=%(id)s"
+        query = "DELETE FROM tbl_notices WHERE id=%(id)s"
         cursor.execute(query, {'id': id})
         cnx.commit()
         cnx.close()
@@ -123,4 +122,3 @@ class Notifications:
 if __name__ == '__main__':
     connection_pool = mysql.connector.pooling.MySQLConnectionPool(
         user="root", password="", database='goldis', use_pure=False, pool_name="my_pool", pool_size=32, buffered=True)
-
