419694c1ca72a3b6d0bf9cd87c50145a61adad15
NedaAlipour39 <neda.alipour39@gmail.com>
2023-01-10 09:53:13 +0330

    sfsf

--- aiohttp_dones/dones/routes/notifications_routes.py
+++ aiohttp_dones/dones/routes/notifications_routes.py
@@ -26,11 +26,11 @@ def make_routes(fullstack_blueprint):
         else:
             user_full_name = user['full_name']
         user_notifications = notifications_orm.Notifications.get_all_notifications_by_receiver_id(user['id'])
-        update_notification = notifications_orm.Notifications.update_notification(id = notification_id, is_read=notifications_orm.Notifications.Read_status.read) 
+        update_notification = notifications_orm.Notifications.update_notification(id = notification_id, is_read=notifications_orm.Notifications.Read_status.read.value) 
         notification = notifications_orm.Notifications.get_notification_by_id_and_user_id(user['id'], notification_id)
         if notification == None:
             flash(f'{user_full_name} گرامی پیامی برای نمایش وجود ندارد.', 'danger')
             return redirect('/notifications')
         notification['jalali_date'] = date_converter.Date_converter.unix_timestamp_to_jalali(notification['unix_datetime'])
         unread_notifications_count = notifications_orm.Notifications.get_unread_notifications_count_by_receiver_id(user['id'])[0]
-        return render_template('notifications.html', user=user, notification=notification, user_notifications=user_notifications, unread_notifications_count=unread_notifications_count)
+        return render_template('notification.html', user=user, notification=notification, user_notifications=user_notifications, unread_notifications_count=unread_notifications_count)
